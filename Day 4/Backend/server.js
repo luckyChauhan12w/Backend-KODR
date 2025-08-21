1731a51ad6cd767b7e3b308bc8a4a597bab857a7
Lucky Chauhan <luckychauhan.12w@gmail.com>
2025-08-21 21:13:25 +0530

    add env.example file

--- Day 4/Backend/server.js
+++ Day 4/Backend/server.js
@@ -1,8 +1,9 @@
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
+require("dotenv").config();
 
 connectDB();
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server is running on port 3000');
 });
